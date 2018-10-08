334a414f0ecc5e26d2371d366b3d53982770eb95
gaopeng <you@example.com>
2018-10-08 17:39:04 +0800

    新增Dom增删节点

--- 第二周/utils.js
+++ 第二周/utils.js
@@ -1,16 +1,21 @@
-var utils = {
-    //listToArray:实现将类数组转换为数组
-    listToArray:function(likeAry){
+//使用惰性思想(JS高阶编程技巧之一) 来封装我们的方法库：在第一次给我们的utils的时候就已经把兼容处理好了，把最后的结果存放在flag中
+//以后再每一个方法中只要是IE6-8不兼容的，我们不需要重新检测，只需要flag的值即可
+var utils =(function(){
+    var flag = "getComputedStyle" in window;//flag这个变量不销毁，存储的是判断当前的浏览器是否兼容getComputedStyle，兼容
+    // 的话就是标准浏览器，不兼容就是IE6-8
+
+     //listToArray:实现将类数组转换为数组
+     function listToArray(likeAry){
+        if(flag){
+            return Array.prototype.slice.call(likeAry);
+        }
         var ary = [];
-        try{
-            ary = Array.prototype.slice.call(likeAry);
-        }catch(e){
-            for(var i=0;i<likeAry.length;i++){
-                ary[ary.length]=likeAry[i]
-            }
+        for(var i=0;i<likeAry.length;i++){
+            ary[ary.length]=likeAry[i]
         }
         return ary;
-    },
+    }
+
     //把JSON格式的字符串转换为json格式的对象
     // jsonParse:function(str){
     //     var val = null;
@@ -21,11 +26,11 @@ var utils = {
     //     }
     //     return val;
     // }
-    jsonParse: function (jsonStr){
+    function jsonParse(jsonStr){
         return 'JSON' in window ? JSON.parse(jsonStr) : eval("(" + jsonStr+")");
-    },
-    //获取地址栏里拼接的参数
-    queryUrlParameter:function(str){
+    }
+        //获取地址栏里拼接的参数
+    function queryUrlParameter(str){
         var reg = /([^?=&]+)=([^?=&]+)/g;
         var res = reg.exec(str);
         var obj = {};
@@ -34,9 +39,9 @@ var utils = {
             res = reg.exec(str);
         }
         return obj;
-    },
+    }
     //万能时间格式化
-    myFormatTime:function(str){
+    function myFormatTime(str){
         var reg = /^(\d{4})(?:-|\/|\.|:)(\d{1,2})(?:-|\/|\.|:)(\d{1,2})(?:\s+)(\d{1,2})(?:-|\/|\.|:)(\d{1,2})(?:-|\/|\.|:)(\d{1,2})$/g
         var ary = [];
         if(typeof str === "undefined"){
@@ -53,9 +58,9 @@ var utils = {
         return format.replace(/{(\d+)}/g,function(){
             return ary[arguments[1]];
         });
-    },
+    }
     // 字符串中出现字数最多的字母
-    strMax:function(str){
+    function strMax(str){
         var obj={};
         var reg = /[a-z]/g
         str.replace(reg,function(){
@@ -71,37 +76,37 @@ var utils = {
             obj[key] === maxNum ? ary.push(key) : null
         }
         return "整个字符串当中出现次数最多的字符是："+ary.toString()+"，出现了"+maxNum+"次";
-    },
+    }
     //设置cookie
-    setCookie: function(obj, time){
+    function setCookie(obj, time){
         time = new Date(Date.now() + (time ? time : 0) * 365*24*60*60*1000).toUTCString();
         for(var key in obj){
             document.cookie = key + "=" + obj[key] + ';expires=' + time;
         }
-    },
+    }
     //获取cookie
-    getCookie:function(attr){
+    function getCookie(attr){
         var arr = document.cookie.match(new RegExp('\\b' + attr + '=([^;]+)(;|$)'));
         return arr ? arr[1] : "";
-    },
+    }
     //移除cookie
-    removeCookie:function(attr){
+    function removeCookie(attr){
         var obj = {};
         obj[attr] = "";
         setCookie(obj, -1)
-    },
+    }
     //win:编写一个有关于操作浏览器盒子模型的方法
-    win:function(attr,value){
+    function win(attr,value){
         if(typeof attr==="undefined"){
-           return document.documentElement[attr] || document.body[attr]
+            return document.documentElement[attr] || document.body[attr]
         }
         document.documentElement[attr] = value;
         document.body[attr] = value;
-    },
+    }
     //获取当前元素所有经过浏览器计算过的样式中的attr对应的值
-    getCss:function (curEle,attr){
+    function getCss(curEle,attr){
         var val = null,reg=null;
-        if("getComputedStyle" in window){ //->返回的是true，说明window下有这个属性，代表兼容
+        if(flag){ //->返回的是true，说明window下有这个属性，代表兼容
             val = window.getComputedStyle(curEle,null)[attr];
         }else{ // IE 6-8
             //如果传递进来的是opacity，说明我想获取到的是透明度，但是在IE6-8下获取透明度需要使用filter
@@ -116,15 +121,144 @@ var utils = {
         reg = /^-?\d+(\.\d+)?(px|pt|rem|em)?$/i;
         return reg.test(val) ? parseFloat(val) : val; //这样写肯定不行，对于某些样式属性的值是不能去单位的，例如:float,position,margin,padding,border这些，backgroundColor...
     }
+    //获取当前元素下所有元素节点,如果多传递一个标签名的话，我们还要在获取的元素集合中把对应标签名的进行二次筛选
+    function children(curEle,tagName){
+    var ary = [];
+    /* IE6-8不能使用内置的children属性，我们自己代码实现 */
+    if(!flag){
+        var nodeList = curEle.childNodes;
+        for(var i=0,len=nodeList.length;i<len;i++){
+            var curNode = nodeList[i];
+            if(curNode.nodeType ===1){
+                ary[ary.length] = curNode;
+            }
+        }
+    }else{
+        //->标准浏览器，我们直接使用children即可，但是这样获取的是一个类数组集合，为了和IE6-8
+        //下保持一致，我们借用数组原型上的slice，把类数组转化为数组
+        ary = Array.prototype.slice.call(curEle.children);
+    }
+    //二次筛选
+    if(typeof tagName === "string"){
+        for(var k = 0; k < ary.length;k++){
+            var curEleNode = ary[k];
+            if(curEleNode.nodeName.toLowerCase()!==tagName.toLowerCase()){
+                //不是我想要的那个标签
+                ary.splice(k,1);
+                k--;
+            }
+        }
+    }
+    return ary;
+    }
+
+    //prev 获取上一个哥哥元素节点
+    //->首先获取当前元素的上一个哥哥节点，判断是否为元素节点，不是的话基于当前的继续找上门的哥哥节点。。。
+    //一直到找到哥哥元素节点为止，如果没有返回null即可
+    function prev(curEle){
+        if(flag){
+            return curEle.previousElementSibling;          
+        }
+        var pre = curEle.previousSibling;
+        console.log(pre)
+        while(pre&&pre.nodeType!==1){ //不等于1说明不是元素节点
+            pre =pre.previousSibling ;
+        }
+        // return pre;
+    }
+
+    //获取下一个弟弟元素节点
+    function next(curEle){
+        if(flag){
+            console.log(curEle.nextSibling)
+            return curEle.nextElementSibling;     
+        }
+         var nex = curEle.nextSibling;
+         
+         if(nex && nex.nodeType !== 1){
+            nex = curEle.nextiousSibling;
+         }
+         return nex;
+    }
+    //prevAll:获取所有的哥哥元素节点
+    function prevAll(curEle){
+        var ary = [];
+        var pre = this.prev(curEle);
+        while(pre){
+            ary.unshift(pre);//放在开头
+            pre = this.prev(pre);
+        }
+        return ary;
+    }
+    //nextAll获取所有的弟弟元素节点
+    function nextAll(curEle){
+        var ary = [];
+        var nex = this.next(curEle);
+        while(nex){
+            ary.push(nex);
+            nex = this.next(nex);
+        }
+        return ary;
+    }
+
+    //silbing:获取相邻的两个元素节点
+    function sibling(curEle){
+        var ary=[];
+        var pre = this.prev(curEle);
+        var nex = this.next(curEle);
+        pre?ary.push(pre):null;
+        nex?ary.push(nex):null;
+        return ary
+    }
+    //siblings:获取所有的兄弟元素节点
+    function siblings(curEle){
+        return this.prevAll(curEle).concat(this.nextAll(curEle));
+    }
+    //index 获取当前元素索引
+    function index(curEle){
+        return this.prevAll(curEle).length;
+    }
+
+    //firstChild 获取第一个元素子节点
+    function firstChild(curEle){
+        var chs = this.children(curEle);
+        return chs.length > 0 ? chs[0] : null;
+    }
+
+     //lastChild 获取最后一个元素子节点
+     function lastChild(curEle){
+        var chs = this.children(curEle);
+        return chs.length > 0 ? chs[chs.length-1] : null;
+    }
 
 
 
 
 
+    return  {
+        listToArray:listToArray,
+        jsonParse:jsonParse,
+        queryUrlParameter:queryUrlParameter,
+        myFormatTime:myFormatTime,
+        strMax:strMax,
+        getCookie:getCookie,
+        setCookie:setCookie,
+        removeCookie:removeCookie,
+        win:win,
+        getCss:getCss,
+        children:children,
+        prev:prev,
+        next:next,
+        prevAll:prevAll,
+        nextAll:nextAll,
+        sibling:sibling,
+        siblings:siblings,
+        index:index,
+        firstChild:firstChild,
+        lastChild:lastChild
+    }
 
 
 
 
-
-
-}
+})();
